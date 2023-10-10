b4ab6aa7f5b3f02dc8c9acbaeb46b36694c9d0c9
Max <7510332@gmail.com>
2023-10-10 12:14:02 +0300

    slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
+    infinite: false,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
   });
