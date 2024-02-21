0b469376d1eb73056c0347be13aaf33da610361f
Max <7510332@gmail.com>
2024-02-21 10:15:46 +0200

    script для швидкого повернення у верхню частину

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
-  $('.carousel__inner').slick({
-    speed: 1200,
-    infinite: true,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
-  });
- });
+	$('.carousel__inner').slick({
+		speed: 1200,
+		infinite: true,
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>'
+	});
+
+	//smooth scroll and page up
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1200) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr(".href");
+		$("html,body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+ })
